3aedbdd129b703de0c37de7fc07944cbf9db7655
Nitza Alfinas <nitzaalfinas@gmail.com>
2017-10-29 08:29:08 +0700

    wallet UI and post action

--- routes/wallet.js
+++ routes/wallet.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+var cryptico = require('cryptico');
+
 router.get('/create', function(req, res, next) {
     res.render('wallet/create', {});
+});
+
+/**
+Creating wallet
+
+---
+Process
+The public key and the private key are stored in thedata/key folder
+*/
+router.post('/create_post', function(req, res, next) {
+
+    var rsaKeyPrivate = cryptico.generateRSAKey(req.body.passphrase, 512);
+
+    var rsaKeyPublic = cryptico.publicKeyString(rsaKeyPrivate);
+
+    
 });
 
 module.exports = router;
